f2aa57e966959bab425950e23aeb9337e692cc4b
benn511 <bgomez190@gmail.com>
2020-10-06 20:44:51 -0500

    working start point for quiz app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class QuizBee extends Component {
+  //State variable should never be written to directly by us..Only by API
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0,
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //Invoke api to get our questions(Update State var)
+  getQuestions = () => {
+    quizService().then((question) => {
+      this.setState({ questionBank: question });
+    });
+  };
+
+  // Controls state of score, and responses
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      this.setState({
+        score: this.state.score + 1,
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5,
+    });
+  };
+
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0,
+    });
+  };
+
+  //Function of life cycle...Call this to invoke beginning of life
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="title">Javascript Quiz</div>
+        {/* Renders questions when available..
+        there has to be at least one question to render and less than 5 responses to our questions */}
+        {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
+          this.state.questionBank.map(
+            ({ question, answers, correct, questionId }) => (
+              <QuestionBox
+                question={question}
+                options={answers}
+                key={questionId}
+                selected={(answer) => this.computeAnswer(answer, correct)}
+              />
+            )
+          )}
+
+        {/* Different render state */}
+        {this.state.responses === 5 ? (
+          <Result score={this.state.score} playAgain={this.playAgain} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
