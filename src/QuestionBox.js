b800c4f9c91354f908b088726b3cd9ce3a02c591
Benito Gomez <bgomez@Benitos-MacBook-Pro.local>
2020-12-07 21:13:05 -0600

    added documentation on useOptions() function and states

--- src/QuestionBox.js
+++ src/QuestionBox.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 
 const QuestionBox = ({ question, options, selected, updateClick }) => {
+  /*[theStateValue, stateModifier] = useState(values or value); */
   const [answer, setAnswer] = useState(options);
-  // const [isDisabled, setButton] = useState(false);
+  const [isDisabled, setDisabled] = useState(false);
+
+  const disableButton = () => {
+    setDisabled(true);
+  };
+
   return (
     <div className="questionBox">
       <div className="question">{question}</div>
@@ -11,12 +17,10 @@ const QuestionBox = ({ question, options, selected, updateClick }) => {
           key={index}
           className="answerBtn"
           onClick={() => {
-            //Disable button
-            // setButton(true);
             setAnswer([text]);
             selected(text);
           }}
-          disabled={isDisabled}
+          disabled
         >
           {text}
         </button>
